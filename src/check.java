2b61aab72e6ac1c78db78e1cb522b79d5e473d90
praveenprabhakaran-v <praveenprabhakaran.dev@gmail.com>
2026-08-09 01:23:24 +0530

    Initial commit

---
+++ src/check.java
@@ -0,0 +1,5 @@
+public class check  {
+    static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/hello.java
@@ -0,0 +1,6 @@
+public class hello  {
+    static void main(String[] args) {
+        System.out.println("First Program");
+        System.out.println("Hello World");
+    }
+}
