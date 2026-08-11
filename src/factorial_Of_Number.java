f850d3d58d7b416069b7a3de640f54821957ec3b
praveenprabhakaran-v <praveenprabhakaran.dev@gmail.com>
2026-08-11 13:37:20 +0530

    Q9: Factorial of a Number

---
+++ src/factorial_Of_Number.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class factorial_Of_Number {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int i = 1;
+        while(n > 0){
+            i = i*n;
+            n--;
+        }
+        System.out.println("result:"+ i);
+    }
+    
+}
