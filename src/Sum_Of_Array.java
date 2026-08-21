a9999015bba9d46b6d8ad32bb1db79024b321f6e
praveenprabhakaran-v <praveenprabhakaran.dev@gmail.com>
2026-08-21 12:21:18 +0530

    q: Sum of Array

---
+++ src/Sum_Of_Array.java
@@ -0,0 +1,14 @@
+public class Sum_Of_Array{
+public static void main(String[] args) {
+    int[] arr = {1,2,3,4};
+
+    int sum = 0;
+    for(int i = 0; i<arr.length; i++){
+        sum += arr[i];
+    }
+    System.out.println(sum);
+
+}
+    
+
+}
